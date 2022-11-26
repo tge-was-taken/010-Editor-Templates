89044c9e4de6073d98da8b84e046ba8938b34ae6
Cherry Cream Soda <noahsota@gmail.com>
2022-11-26 18:04:38 -0500

    Add typedef for P5R bitflags for all templates
    This data type is essentially required for p5r filetypes, especially related to fields. Flags from the ps3 game will be passed through the return unchanged.

--- templates/common/utils.h
+++ templates/common/utils.h
@@ -258,5 +258,15 @@ void ArrayMaxUInt( u32 array[], u32 count )
     return max;
 }
 
+typedef int Bitflag<read=FlagConvert(this),open=true>;                         
+int FlagConvert( int flag )
+{
+    if      (flag >= 0x50000000){ flag = (flag - 0x50000000) + 12288; }
+    else if (flag >= 0x40000000){ flag = (flag - 0x40000000) + 11776; }
+    else if (flag >= 0x30000000){ flag = (flag - 0x30000000) + 11264; }
+    else if (flag >= 0x20000000){ flag = (flag - 0x20000000) + 6144;  }
+    else if (flag >= 0x10000000){ flag = (flag - 0x10000000) + 3072;  }
+    return flag;
+}
 
 #endif // #ifndef UTILS_H
